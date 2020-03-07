c2c9584c29506f2dfb122a1a35844cc607fb2150
Maile Pedersen <maybemaile@gmail.com>
2020-03-07 00:08:01 -0800

    scroll to target using smooth .scrollIntoView().

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -35,21 +35,27 @@ const navItems = document.querySelectorAll('section');
 */
 
 // build the nav
-for (let section of navItems) {
-  li = document.createElement('li');
-  section_link = document.createElement('a');
-  section_link.setAttribute("href", '#' + section.id);
-  section_link.innerText = section.dataset.nav;
-  section_link.className = "menu__link";
-  li.appendChild(section_link);
-  navBar.appendChild(li);
+function buildNav() {
+  for (let section of navItems) {
+    li = document.createElement('li');
+    section_link = document.createElement('a');
+    section_link.innerText = section.dataset.nav;
+    section_link.target_id = section.id;
+    section_link.onclick = scrollToTarget;
+    section_link.className = "menu__link";
+    li.appendChild(section_link);
+    navBar.appendChild(li);
+  }
 }
 
 // Add class 'active' to section when near top of viewport
 
 
 // Scroll to anchor ID using scrollTO event
-
+function scrollToTarget() {
+  target_el = document.querySelector("#" + this.target_id);
+  target_el.scrollIntoView({behavior: "smooth"});
+}
 
 /**
  * End Main Functions
@@ -58,7 +64,10 @@ for (let section of navItems) {
 */
 
 // Build menu
+document.onload = buildNav();
 
 // Scroll to section on link click
+// onclick for menu__link ?
 
 // Set sections as active
+// event??
