8a47fa5df669122b13168311c15f128c2e71526b
Maile Pedersen <maybemaile@gmail.com>
2020-04-03 22:43:57 -0700

    kick off weather journal

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -2,18 +2,37 @@
 projectData = {};
 
 // Require Express to run server and routes
+let express = require('express');
+let cors = require('cors');
+let bodyParser = require('body-parser');
 
 // Start up an instance of app
+app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Cors for cross origin allowance
-
 // Initialize the main project folder
 app.use(express.static('website'));
 
+// set up routes
+app.get('/weather', getWeather);
+app.post('/weather', postWeather);
+
+function getWeather(req, res) {
+    res.send(projectData);
+}
+
+function postWeather(req, res) {
+    let data = req.body;
+    projectData['temperature'] = data['temperature'];
+    projectData['date'] = data['date'];
+    projectData['mood'] = data['mood'];
+}
 
 // Setup Server
+app.listen(3000, (req, res) => {
+  console.log('server running!');
+});
