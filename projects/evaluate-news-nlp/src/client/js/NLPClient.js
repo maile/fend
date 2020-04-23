ec3a28c1e9e53df4afb558ff9819f465c6691791
Maile Pedersen <maybemaile@gmail.com>
2020-04-22 21:45:10 -0700

    change post target as per review

--- projects/evaluate-news-nlp/src/client/js/NLPClient.js
+++ projects/evaluate-news-nlp/src/client/js/NLPClient.js
@@ -15,7 +15,7 @@ async function postData(url = '', data = {}) {
 
 async function requestSentiment(url) {
     console.log("::: Running requestSentiment :::", url);
-    return postData('/sentiment', { 'url': url}).catch(err => console.log('failed to request sentiment'));
+    return postData('http://localhost:8080/sentiment', { 'url': url}).catch(err => console.log('failed to request sentiment'));
 }
 
 export { requestSentiment, postData }
